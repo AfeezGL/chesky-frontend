d1841edec2a563cde0d34fd47b70baf46ce9e2c7
Afeez G Lawal <afeezlg@gmail.com>
2023-02-17 17:37:37 +0100

    chore: renamed contact us to contact us section

--- homepage/ContactUsSection.jsx
+++ homepage/ContactUsSection.jsx
@@ -15,7 +15,7 @@ const contactDetails = [
   },
 ];
 
-export default function ContactUs() {
+export default function ContactUsSection() {
   return (
     <section className='mt-16 mb-24' id='contact-us' aria-label='Contact us'>
       <h2 className='font-bold text-blue text-3xl text-center capitalize pb-14'>
