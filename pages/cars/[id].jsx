b1fa6ee8aadc6efa90bc9b78717f5c406ecebbc0
Afeez G Lawal <afeezlg@gmail.com>
2023-02-18 06:52:28 +0100

    feat: adds car features

--- pages/cars/[id].jsx
+++ pages/cars/[id].jsx
@@ -1,7 +1,20 @@
+import CarImg from '@/assets/cars/car-img.svg';
+import Car from '@/cars/partials/Car';
 import { useRouter } from 'next/router';
 
 export default function CarDetails() {
   const router = useRouter();
   const { id } = router.query;
-  return <div>{id}</div>;
+  const car = { id, name: 'Toyota Corolla', image: CarImg, price: 40 };
+  return (
+    <section
+      aria-label='Car details'
+      className='flex gap-9 flex-col sm:flex-row'
+    >
+      <div className='sm:basis-2/3'>
+        <Car car={car} showPrice={false} />
+      </div>
+      <div>cost section</div>
+    </section>
+  );
 }
