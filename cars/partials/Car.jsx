91c5df6619c2896227a40f03d8008738d13f9566
Afeez G Lawal <afeezlg@gmail.com>
2023-02-18 06:49:52 +0100

    feat: makes car component reusable

--- cars/partials/Car.jsx
+++ cars/partials/Car.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import CarFeatures from './Carfeatures';
 
-export default function Car({ car }) {
+export default function Car({ car, showPrice = true }) {
   return (
     <div className='flex gap-6 bg-chesky-gray bg-opacity-5 rounded-xl mb-5 last-of-type:mb-0'>
       <Image src={car.image} alt={car.name} />
@@ -12,22 +12,26 @@ export default function Car({ car }) {
           <CarFeatures />
         </div>
         <div className='flex gap-4 flex-col justify-center items-center px-6'>
-          <div className='text-blue text-center'>
-            <span className='block'>Price:</span>
-            <strong className='text-3xl font-normal'>
-              {car.price.toLocaleString('en-us', {
-                style: 'currency',
-                currency: 'USD',
-                minimumFractionDigits: 2,
-              })}
-            </strong>
-          </div>
-          <Link
-            href={`/cars/${car.id}`}
-            className='bg-blue py-3 px-14 text-white text-lg rounded-lg'
-          >
-            Rent
-          </Link>
+          {showPrice && (
+            <>
+              <div className='text-blue text-center'>
+                <span className='block'>Price:</span>
+                <strong className='text-3xl font-normal'>
+                  {car.price.toLocaleString('en-us', {
+                    style: 'currency',
+                    currency: 'USD',
+                    minimumFractionDigits: 2,
+                  })}
+                </strong>
+              </div>
+              <Link
+                href={`/cars/${car.id}`}
+                className='bg-blue py-3 px-14 text-white text-lg rounded-lg'
+              >
+                Rent
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
