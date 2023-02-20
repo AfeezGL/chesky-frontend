357887d9d7387b68d1d598773caffba9660eca03
Afeez G Lawal <afeezlg@gmail.com>
2023-02-20 13:28:58 +0100

    chore: refactors cars page

--- pages/cars/index.jsx
+++ pages/cars/index.jsx
@@ -1,14 +1,42 @@
-import CarList from '@/cars/CarList';
+import Car from '@/cars/partials/Car';
 import SearchBar from '@/components/SearchBar';
 import SideBar from '@/sidebar/SideBar';
+import axiosClient from '@/utils/axios';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useMutation } from 'react-query';
 
 export default function Index() {
+  const router = useRouter();
+  const { pickupDateTime, dropOffDateTime, pickupLocation } = router.query;
+
+  const mutation = useMutation(async (payload) => {
+    const res = await axiosClient.post('/search', payload);
+
+    return await res.data;
+  });
+  const { isLoading, isSuccess, isError, data } = mutation;
+
+  useEffect(() => {
+    if (pickupDateTime && dropOffDateTime && pickupLocation && window) {
+      mutation.mutate({
+        pickupDateTime,
+        dropOffDateTime,
+        pickupLocation,
+      });
+    }
+  }, [pickupDateTime, dropOffDateTime, pickupLocation]);
+
   return (
     <>
       <SearchBar />
       <div className='py-10 sm:flex sm:gap-8'>
         <SideBar />
-        <CarList />
+        <div className='px-3 sm:px-0 w-full'>
+          {isLoading && 'Loading...'}
+          {isError && <div>Failed to load</div>}
+          {isSuccess && data.data.map((car) => <Car car={car} key={car.id} />)}
+        </div>
       </div>
     </>
   );
