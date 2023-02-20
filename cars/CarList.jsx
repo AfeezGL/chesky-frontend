bdbe6ee758aa4543426a118cb96cc7c4ef7e171a
Afeez G Lawal <afeezlg@gmail.com>
2023-02-20 02:43:25 +0100

    feat: fetching data from the backend

--- cars/CarList.jsx
+++ cars/CarList.jsx
@@ -1,19 +1,32 @@
-import CarImg from '@/assets/cars/car-img.svg';
+import axiosClient from '@/utils/axios';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useMutation } from 'react-query';
 import Car from './partials/Car';
 
-const carlist = [
-  { id: 1, name: 'Toyota Corolla', image: CarImg, price: 40 },
-  { id: 2, name: 'Toyota Corolla', image: CarImg, price: 40 },
-  { id: 3, name: 'Toyota Corolla', image: CarImg, price: 40 },
-  { id: 4, name: 'Toyota Corolla', image: CarImg, price: 40 },
-  { id: 5, name: 'Toyota Corolla', image: CarImg, price: 40 },
-];
 export default function CarList() {
+  const router = useRouter();
+  const { pickupDateTime, dropOffDateTime, pickupLocation } = router.query;
+
+  const mutation = useMutation(async (payload) => {
+    const res = await axiosClient.post('/search', payload);
+    return res.data;
+  });
+
+  const { isLoading, isSuccess, data, mutate } = mutation;
+
+  useEffect(() => {
+    mutate({
+      pickupDateTime,
+      dropOffDateTime,
+      pickupLocation,
+    });
+  }, []);
+
   return (
     <div className='px-3 sm:px-0 w-full'>
-      {carlist.map((car) => (
-        <Car car={car} key={car.id} />
-      ))}
+      {isLoading && 'Loading...'}
+      {isSuccess && data.data.map((car) => <Car car={car} key={car.id} />)}
     </div>
   );
 }
