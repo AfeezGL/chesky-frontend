3652f35f7151c36b3fe4fd1f9564e3441b90b3b0
Afeez G Lawal <afeezlg@gmail.com>
2023-02-21 15:59:40 +0100

    feat: integrates car details page

--- pages/cars/[id].jsx
+++ pages/cars/[id].jsx
@@ -1,7 +1,28 @@
 import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import axiosClient from '@/utils/axios';
+import CarDetailsScreen from '@/cars/CarDetailsScreen';
 
 export default function CarDetailsPage() {
   const router = useRouter();
   const { id } = router.query;
-  return <div>{id}</div>;
+
+  const { isLoading, isSuccess, isError, data, error } = useQuery(
+    ['car'],
+    async () => {
+      const res = await axiosClient.get(`/car/${id}`);
+      return await res.data;
+    },
+    { enabled: !!id }
+  );
+
+  return (
+    <>
+      {isLoading && 'Loading...'}
+      {isSuccess && data.data ? <CarDetailsScreen car={data.data} /> : null}
+      {isSuccess && !data.data
+        ? "The car you're looking for is no longer available."
+        : null}
+    </>
+  );
 }
