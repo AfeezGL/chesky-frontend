2d73cc6551c555e7d48bb9424df2be628e3eb693
Afeez G Lawal <afeezlg@gmail.com>
2023-02-21 10:58:33 +0100

    feat: adds error interceptor to axios client

--- utils/axios.js
+++ utils/axios.js
@@ -4,4 +4,17 @@ const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status.errors[0].details)
+      throw new Error(error.response.status.errors[0].details);
+
+    if (error.response.data.detail) {
+      throw new Error(error.response.data.detail);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosClient;
