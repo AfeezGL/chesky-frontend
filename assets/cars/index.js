b06da991fe0b5c8556d07dd74fe010c3a1987399
Afeez G Lawal <afeezlg@gmail.com>
2023-02-25 16:24:13 +0100

    fix: removes mileage and fuel type from car features

--- assets/cars/index.js
+++ assets/cars/index.js
@@ -1,5 +1,3 @@
 export { DoorIcon } from './DoorIcon';
-export { MileageIcon } from './MileageIcon';
-export { PetrolIcon } from './PetrolIcon';
 export { TransmissionIcon } from './TransmissionIcon';
 export { WifiIcon } from './WifiIcon';
