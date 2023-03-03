c7a73aa37140d53b9ee80df3fdb934beb3592f98
Afeez G Lawal <afeezlg@gmail.com>
2023-03-03 17:21:18 +0100

    chore: styles the login screen

--- components/auth/LoginScreen.jsx
+++ components/auth/LoginScreen.jsx
@@ -20,8 +20,11 @@ export const LoginScreen = () => {
   };
 
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
-      <form onSubmit={handleSubmit} className='flex w-28'>
+    <div className='flex justify-center items-center h-[80vh] w-full'>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col w-full max-w-lg gap-2 rounded shadow-md p-4'
+      >
         <label htmlFor='email'>Email:</label>
         <input
           type='email'
@@ -29,6 +32,7 @@ export const LoginScreen = () => {
           id='email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          className='rounded p-2 border-2 border-gray'
         />
         <label htmlFor='password'>Password:</label>
         <input
@@ -37,8 +41,13 @@ export const LoginScreen = () => {
           id='password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className='rounded p-2 border-2 border-gray'
         />
-        <input type='submit' value='Login' />
+        <input
+          type='submit'
+          value='Login'
+          className='bg-blue p-2 rounded text-white'
+        />
       </form>
     </div>
   );
